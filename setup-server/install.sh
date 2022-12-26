14504a65d039e2f91a0ae4168cfde4a8c9bf2ca6
Henry Tran <henry.tran@glife.com.sg>
2022-12-26 18:09:36 +0700

    add install font files script

--- setup-server/install.sh
+++ setup-server/install.sh
@@ -5,13 +5,16 @@ if [ "$(whoami)" != "app" ]; then
 fi
 
 # Essentials
-sudo apt-get -y install software-properties-common apt-transport-https autoconf bison build-essential libssl-dev libyaml-dev libreadline-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm-dev nginx nginx-extras nginx-common
+sudo apt-get -y install unzip software-properties-common apt-transport-https autoconf bison build-essential libssl-dev libyaml-dev libreadline-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm-dev nginx nginx-extras nginx-common
 
 # Nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+source ~/.bashrc
 nvm install 16.15.1
 nvm use 16.15.1
 npm install -g pm2 yarn
@@ -31,6 +34,10 @@ echo "mongodb-org-tools hold" | sudo dpkg --set-selections
 # wkhtmltopdf
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
 sudo apt -y install ./wkhtmltox_0.12.6-1.focal_amd64.deb
+
+wget https://github.com/GlifeRnD/GlifeDeploymentScripts/raw/master/fonts/pingfang.zip
+mkdir -p ~/.local/share/fonts
+unzip ./pingfang.zip -d ~/.local/share/fonts
 
 # Redis
 sudo apt-get install -y redis-server
