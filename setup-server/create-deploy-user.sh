2494f7bf894995703a7427412800843bc6e0e3e8
Henry Tran <henry.tran@glife.com.sg>
2022-12-26 15:39:16 +0700

    update create user script

--- setup-server/create-deploy-user.sh
+++ setup-server/create-deploy-user.sh
@@ -1,7 +1,8 @@
 sudo useradd --p U6aMy0wojraho --create-home --shell "/bin/bash" --user-group --groups "users,sudo" app
+sudo passwd -d app
 USER_HOME=/home/app
 sudo -u "app" mkdir "$USER_HOME/.ssh"
 sudo -u "app" touch "$USER_HOME/.ssh/authorized_keys"
 sudo chmod 0600 "$USER_HOME/.ssh/authorized_keys"
 sudo su app
-cd
+cd ~
